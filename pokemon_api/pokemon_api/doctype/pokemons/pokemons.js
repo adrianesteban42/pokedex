212b3d8d0a53300abe27246e087600cb80da06ab
adrianesteban42 <adrianesteban42@gmail.com>
2024-02-08 17:16:57 +0100

    primer commit

---
+++ pokemon_api/pokemon_api/doctype/pokemons/pokemons.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Adrian Martinez and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("pokemons", {
+// 	refresh(frm) {
+
+// 	},
+// });
